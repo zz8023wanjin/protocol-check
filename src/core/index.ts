7049dae293223e840bd87779e39dbe2ed0b32b82
moyuxin <15984587583@163.com>
2025-05-13 20:12:43 +0800

    chore: 删除未使用代码、增加调试工具

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,22 +1,5 @@
 import { _createHiddenIframe, _registerEvent } from '../helper/dom'
 
-const openUrlUsingFirefox = (url: string, onSuccess: () => void, onError: () => void) => {
-  let iframe: HTMLIFrameElement | null = document.querySelector('#hiddenIframe')
-
-  if (!iframe) {
-    iframe = _createHiddenIframe(document.body, 'about:blank')
-  }
-
-  try {
-    iframe.contentWindow?.location.replace(url)
-    onSuccess()
-  } catch (e: any) {
-    if (e.name === 'NS_ERROR_UNKNOWN_PROTOCOL') {
-      onError()
-    }
-  }
-}
-
 const openUrlWithTimeoutHack = (url: string) => {
   return new Promise((resolve, reject) => {
     const timeout = setTimeout(function () {
@@ -66,4 +49,4 @@ const openUrlWithHiddenFrame = (url: string) => {
   })
 }
 
-export { openUrlUsingFirefox, openUrlWithTimeoutHack, openUrlWithHiddenFrame }
+export { openUrlWithTimeoutHack, openUrlWithHiddenFrame }
